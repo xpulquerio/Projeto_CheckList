326fe1d71668a36773ecbc749fb1009a0e52f7b7
Ewerson Pulquerio <pulquerio.x@gmail.com>
2022-05-25 10:13:16 -0300

    Adicionado a logo

--- checklist/src/components/Menu.js
+++ checklist/src/components/Menu.js
@@ -2,14 +2,19 @@ import React from 'react'
 import styles from './Menu.module.css'
 import Panel from './Panel.js'
 
-const Menu = ({activeMenu, changeMenu}) => {
+const Menu = ({ activeMenu, changeMenu }) => {
   return (
-    <div>
+    <div className={styles.App_Menu}>
+      <div className={styles.App_Menu_logo}>
+      <img src="./logo_check_list.png" />
+      </div>
+      <div>
         <ul className={styles.App_Menu_ul}>
-            <li onClick={() => changeMenu(1)} className={styles.App_Menu_li}>INÍCIO</li>
-            <li onClick={() => changeMenu(2)} className={styles.App_Menu_li}>SOBRE</li>
-            <li onClick={() => changeMenu(3)} className={styles.App_Menu_li}>PAINEL</li>
+          <li onClick={() => changeMenu(1)} className={styles.App_Menu_li}>INÍCIO</li>
+          <li onClick={() => changeMenu(2)} className={styles.App_Menu_li}>SOBRE</li>
+          <li onClick={() => changeMenu(3)} className={styles.App_Menu_li}>PAINEL</li>
         </ul>
+      </div>
     </div>
   )
 }
